65e5ecc38c76389956992cd2b9f0bd373bfbaa2b
nikhitha2206 <63452650+nikhitha2206@users.noreply.github.com>
2022-03-20 16:35:43 +0530

    Add files via upload

---
+++ Desktop Assistant/Speech to text.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Alexa: Listening...")
+        audio=r.listen(source)
+        try:    
+            query = r.recognize_google(audio)
+            print(f"master:{query}")
+            return query
+        except:
+            print("Try Again")
+print(command())
